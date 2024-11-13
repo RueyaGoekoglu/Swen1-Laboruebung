4b015d427ed1feb7d7c11c7f0e433fd9635e45c5
Ralf Schimkat <rschimka@htwg-konstanz.de>
2024-11-13 21:36:20 +0100

    compile is working

--- src/main/java/de/htwg/Bmi.java
+++ src/main/java/de/htwg/Bmi.java
@@ -2,7 +2,7 @@ package de.htwg;
 
 public class Bmi {
 
-    public static double calculateBmi(double weight, double height) {
+    public static double calc(double weight, double height) {
         return weight / (1.2 * height);
     }
 
