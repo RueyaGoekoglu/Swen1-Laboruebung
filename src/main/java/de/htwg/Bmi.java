4792dd9b57041e289708a697d456d34a77035c4f
Rüya Gökoglu <rueya.goekoglu@htwg-konstanz.de>
2024-11-18 14:34:50 +0100

    Bugfix Berechnung Bmi korrigiert

--- src/main/java/de/htwg/Bmi.java
+++ src/main/java/de/htwg/Bmi.java
@@ -3,7 +3,7 @@ package de.htwg;
 public class Bmi {
 
     public static double calc(double weight, double height) {
-        return weight / (1.2 * height);
+        return weight / (height * height);
     }
 
 }
