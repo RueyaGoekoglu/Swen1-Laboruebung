5a5cedea68d577a9b786856ddbda1c4e58b6f3bc
Rüya Gökoglu <rueya.goekoglu@htwg-konstanz.de>
2024-11-18 21:11:45 +0100

    Validierung negative Zahlen

--- src/main/java/de/htwg/Bmi.java
+++ src/main/java/de/htwg/Bmi.java
@@ -3,6 +3,12 @@ package de.htwg;
 public class Bmi {
 
     public static double calc(double weight, double height) {
+        if (weight < 0) {
+            throw new IllegalArgumentException("Weight can´t be negative");
+        }
+        if (height < 0) {
+            throw new IllegalArgumentException("height can´t be negative");
+        }
         return weight / (height * height);
     }
 
